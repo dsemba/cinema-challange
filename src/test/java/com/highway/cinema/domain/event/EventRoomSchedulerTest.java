a139b19410d4fead33ffe8dcb5141b4ab49b69b1
Dariusz Semba <dariusz.semba@gmail.com>
2022-08-20 17:13:42 +0200

    Add TODO to test efficienty with and without 'synchronized'

---
+++ src/test/java/com/highway/cinema/domain/event/EventRoomSchedulerTest.java
@@ -0,0 +1,21 @@
+package com.highway.cinema.domain.event;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class EventRoomSchedulerTest {
+
+    @Ignore("Not implemented yet")
+    @Test(timeout = 1000)
+    public void testEfficiency() {
+        //TODO: Schedule a lot of events lock per day and room
+        //given
+        //a number of events with random room (5) and random datetime within next 2 weeks and a movie
+
+        //when
+        //schedule events ignore errors caused by overlapping
+
+        //then
+        //finished in most efficient time
+    }
+}
